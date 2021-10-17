223ab8a4050bbeb738f1fd29346b28fcaaa6b52f
Robson <robccorreia@gmail.com>
2021-10-17 14:36:10 -0300

    feat: finish stepOne component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import * as C from './styles';
 export const Header = () => {
   return (
     <C.Container>
-      <h1>Cadastrp de Desenvolvedor</h1>
+      <h1>Cadastro de Desenvolvedor</h1>
       <p>Faça seu cadastro na lista e receba um emprego instantaneamente.</p>
     </C.Container>
   )
--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,11 +1,48 @@
-import * as C from './styles';
+import { ChangeEvent, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Theme } from '../../components/Theme';
+import { useForm, FormActions } from '../../contexts/FormContext'
+import * as C from './styles';
 
 export const FormStep1 = () => {
+  const history = useHistory();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1
+    })
+  }, [dispatch]);
+
+  const handleNextStep = () => {
+    if(state.name.length > 0) {
+      history.push('/step2');
+    } else {
+      alert('Preencha os dados.')
+    }
+  }
+
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: event.target.value
+    });
+  }
+
   return (
     <Theme>
       <C.Container>
-        Passo 1
+        <p>Passo {state.currentStep}/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo.</p>
+
+        <hr />
+        <label>Seu nome completo
+          <input type="text" value={state.name} onChange={handleNameChange} autoFocus/>
+        </label>
+
+        <button onClick={handleNextStep}>Próximo</button>
       </C.Container>
     </Theme>
   )
